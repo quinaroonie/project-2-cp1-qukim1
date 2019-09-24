69e8092d8f2c219e94fe66f5b5242c9e7113e313
Ubuntu <ubuntu@ip-172-31-24-233.ec2.internal>
2019-09-24 00:57:33 +0000

    first commit

--- scripts/Content.js
+++ scripts/Content.js
@@ -1,8 +1,35 @@
     
 import * as React from 'react';
+import { Socket } from './Socket';
+
+
+
 
 export class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'data': '',
+        };
+    }
+    
+    componentDidMount() {
+        Socket.on('update', (data) => {
+            this.setState(data);
+        });    
+    }
+
     render() {
-        return <div><h1>Hello World from React!</h1></div>;
+        return (
+            <div>
+                <h1>Hello from React!</h1>
+                <div>
+                    Data: {this.state.data}
+                </div>
+            </div>
+        );
     }
 }
+
+
+
